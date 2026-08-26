61a7a3a6c6d6c8548c30b5379c70e1a1f4c58c06
Charles Michael Cayno <Charles.Cayno@power.com>
2026-08-26 17:52:40 +0800

    feat: precompile tailwind css for faster loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./js/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./*.{html,js}", "./js/**/*.{js,mjs}"],
   theme: {
-    extend: {
-      colors: {
-        'spider-blue': '#222842',
-        'spider-light': '#2c3353',
-        'spider-dark': '#15192b'
-      },
-      fontFamily: {
-        sans: ['"Inter"', 'sans-serif'],
-      },
-      animation: {
-        'float': 'float 6s ease-in-out infinite',
-        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-10px)' },
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
